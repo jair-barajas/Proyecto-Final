02e84376cf8720a691685d386fdc8ded7919b3b7
jair-barajas <56610845+jair-barajas@users.noreply.github.com>
2022-05-27 21:00:28 -0500

    Add files via upload

---
+++ Proyecto Final SSPTL 2/tests/prueba 2 en C.c
@@ -0,0 +1,20 @@
+int x;
+float a;
+float b;
+float c;
+
+int main(int a, int b){
+    a = 20;
+    b = 30;
+    if(a == 20){
+        return a;
+    }
+}
+void i(){
+    while(x){
+        return b;
+    }
+    if(a && 10){
+        c = main(a,b);
+    }
+}
